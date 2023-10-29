79c35e6e361e44c004cc126a1d288a10b4ca89b7
jaccen <jaccen2007@163.com>
2023-10-29 15:59:05 +0800

    增加texture

--- src/Texture.cpp
+++ src/Texture.cpp
@@ -1,10 +1,142 @@
 #include "Texture.h"
 
-Texture::Texture()
-{
+Texture::~Texture() {}
+Texture::Texture(unsigned int ourTexture[], int ourLimit) {
+  limite = ourLimit;
+  for (int i = 0; i < limite; i++) {
+    texture[i] = ourTexture[i];
+  }
 }
 
+void Texture::GeneraTextura(int n, string path, int e) {
+  glGenTextures(1, &texture[n]);
+  glBindTexture(GL_TEXTURE_2D, texture[n]);
 
-Texture::~Texture()
-{
+  if (e == 0) {
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  } else if (e == 1) {
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+  } else if (e == 2) {
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  } else if (e == 3) {
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+
+    float borderColor[] = {0.0f, 0.0f, 0.0f, 0.0f};
+    glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+  } else {
+    cout << "Valores de 0 a 3 en r" << endl;
+  }
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+                  GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  int width, height, nrChannels, nrComponents;
+  stbi_set_flip_vertically_on_load(true);
+  unsigned char* data =
+      stbi_load(path.c_str(), &width, &height, &nrComponents, 0);
+
+  if (data) {
+    GLenum format;
+    if (nrComponents == 1)
+      format = GL_RED;
+    else if (nrComponents == 3)
+      format = GL_RGB;
+    else if (nrComponents == 4)
+      format = GL_RGBA;
+
+    glBindTexture(GL_TEXTURE_2D, texture[n]);
+    glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format,
+                 GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  } else {
+    cout << "No se puede cargar la textura" << endl;
+  }
+  stbi_image_free(data);
+}
+
+void Texture::ViewTexture() {
+  for (int i = 0; i < limite; i++) {
+    glActiveTexture(GL_TEXTURE0 + i);
+    glBindTexture(GL_TEXTURE_2D, texture[i]);
+  }
+}
+
+void Texture::ViewTextureMap(int num, unsigned int nameMap) {
+  glActiveTexture(GL_TEXTURE0 + num);
+  glBindTexture(GL_TEXTURE_2D, nameMap);
+}
+
+string Texture::UniformTexture() {
+  string completo = "";
+  string str = "texture";
+  string numero = "0";
+  int n = stoi(numero);
+  for (int i = 0; i < limite; i++) {
+    completo = "";
+    n += 1;
+
+    numero = to_string(n);
+
+    completo.append(str);
+    completo.append(numero);
+  }
+  return completo;
+}
+
+unsigned int Texture::loadTextureID(string path, int e) {
+  unsigned int textureID;
+  glGenTextures(1, &textureID);
+
+  int width, height, nrComponents;
+  unsigned char* data =
+      stbi_load(path.c_str(), &width, &height, &nrComponents, 0);
+  if (data) {
+    GLenum format;
+    if (nrComponents == 1)
+      format = GL_RED;
+    else if (nrComponents == 3)
+      format = GL_RGB;
+    else if (nrComponents == 4)
+      format = GL_RGBA;
+
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format,
+                 GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    if (e == 0) {
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    } else if (e == 1) {
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+    } else if (e == 2) {
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    } else if (e == 3) {
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+
+      float borderColor[] = {0.0f, 0.0f, 0.0f, 0.0f};
+      glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+    } else {
+      cout << "Valores de 0 a 3 en r" << endl;
+    }
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+                    GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    stbi_image_free(data);
+  } else {
+    cout << "Texture failed to load at path: " << path << endl;
+    stbi_image_free(data);
+  }
+
+  return textureID;
 }
--- src/Texture.h
+++ src/Texture.h
@@ -1,8 +1,31 @@
-#pragma once
-class Texture
-{
-public:
-	Texture();
-	virtual ~Texture();
-};
+#ifndef TEXTURE_H
+#define TEXTURE_H
+#include <gl/gl.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include <iostream>
+#include "stb_image.h"
+using namespace std;
+class Texture {
+ public:
+  Texture() {}
 
+  Texture(unsigned int ourTexture[], int ourLimit);
+
+  void GeneraTextura(int n, string path, int e);
+
+  void ViewTexture();
+
+  void ViewTextureMap(int num, unsigned int nameMap);
+
+  string UniformTexture();
+
+  unsigned int loadTextureID(string path, int e);
+
+ public:
+  unsigned int textureID = 0;
+  unsigned int width = 0;
+  unsigned int hright = 0;
+  int limite;
+  unsigned int texture[1];
+};
+#endif
